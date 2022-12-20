34c6a6ef30ee814b48b627a77ba19535287cb1de
Felipe David <felipedavid.huh@gmail.com>
2022-12-20 11:42:10 -0300

    game loop

--- main.c
+++ main.c
@@ -3,25 +3,34 @@
 #define GAME_NAME "YARP"
 
 LRESULT CALLBACK main_window_proc(_In_ HWND window, _In_ UINT msg, _In_ WPARAM w_param, _In_ LPARAM l_param);
-DWORD create_main_game_window(HINSTANCE instr);
-BOOL game_running(void);
+DWORD create_main_game_window(_Out_ HWND *window, _In_ HINSTANCE inst);
+BOOL instance_already_running(void);
+void ProcessPlayerInput(HWND window);
+
+BOOL game_running = FALSE;
 
 int WinMain(HINSTANCE inst, HINSTANCE prev_inst, PSTR cmd_line, INT cmd_show) {
-    if (game_running()) { 
+    if (instance_already_running()) { 
         MessageBoxA(NULL, "Another instance of this program is already running!", "Error", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
-    if (create_main_game_window(inst) != ERROR_SUCCESS) {
+    HWND window;
+    DWORD error_code = create_main_game_window(&window, inst);
+    if (error_code != ERROR_SUCCESS) {
         MessageBoxA(NULL, "Window registration failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
-
+    
+    game_running = TRUE;
     MSG msg;
-    while (GetMessageA(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessageA(&msg);
+    while (game_running) {
+        while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) DispatchMessage(&msg);
+        ProcessPlayerInput(window);
+        //RenderFrame();
+        Sleep(1);
     }
+
 
     return 0;
 }
@@ -30,6 +39,7 @@ LRESULT CALLBACK main_window_proc(HWND window, UINT msg, WPARAM w_param, LPARAM 
 
     switch (msg) { 
     case WM_CLOSE:
+        game_running = FALSE;
         PostQuitMessage(0);
         break;
     default: 
@@ -38,7 +48,7 @@ LRESULT CALLBACK main_window_proc(HWND window, UINT msg, WPARAM w_param, LPARAM 
     return 0; 
 } 
 
-DWORD create_main_game_window(_In_ HINSTANCE inst) {
+DWORD create_main_game_window(_Out_ HWND *window, _In_ HINSTANCE inst) {
     WNDCLASSEXA window_class = {
         .cbSize = sizeof(WNDCLASSEXA),
         .style = 0,
@@ -52,14 +62,21 @@ DWORD create_main_game_window(_In_ HINSTANCE inst) {
     };
     if (!RegisterClassExA(&window_class)) return GetLastError();
 
-    HWND window = CreateWindowExA(WS_EX_CLIENTEDGE, window_class.lpszClassName, GAME_NAME, (WS_OVERLAPPEDWINDOW | WS_VISIBLE), 
+    *window = CreateWindowExA(WS_EX_CLIENTEDGE, window_class.lpszClassName, GAME_NAME, (WS_OVERLAPPEDWINDOW | WS_VISIBLE), 
         CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, inst, NULL);
-    if (!window) return GetLastError();
+    if (!*window) return GetLastError();
     
     return ERROR_SUCCESS;
 }
 
-BOOL game_running(void) {
+// Let us know if only one instance of the program is running at a given time
+BOOL instance_already_running(void) {
     CreateMutexA(NULL, FALSE, GAME_NAME "_MUTEX");
     return GetLastError() == ERROR_ALREADY_EXISTS;
+}
+
+void ProcessPlayerInput(HWND window) {
+    if (GetAsyncKeyState(VK_ESCAPE)) {
+        SendMessageA(window, WM_CLOSE, 0, 0);
+    }
 }
