bca259a46b7b1d9b1beac284577a6cea3b13d173
Felipe David <felipedavid.huh@gmail.com>
2022-12-20 17:10:31 -0300

    Starting to draw the backbuffer on the screen

--- main.c
+++ main.c
@@ -1,13 +1,33 @@
 #include <windows.h>
+#include <wingdi.h>
 
 #define GAME_NAME "YARP"
+#define GAME_RES_WIDTH 384
+#define GAME_RES_HEIGHT 240
+#define GAME_BPP 32
+#define GAME_DRAWING_AREA_MEM_SIZE (GAME_RES_WIDTH * GAME_RES_HEIGHT * (GAME_BPP/8))
 
 LRESULT CALLBACK main_window_proc(_In_ HWND window, _In_ UINT msg, _In_ WPARAM w_param, _In_ LPARAM l_param);
 DWORD create_main_game_window(_Out_ HWND *window, _In_ HINSTANCE inst);
 BOOL instance_already_running(void);
 void ProcessPlayerInput(HWND window);
+void RenderFrame(HWND window);
+DWORD get_monitor_resolution(int *monitor_width, int *monitor_height, HWND window);
+
+typedef struct Game_Bitmap {
+    BITMAPINFO bitmap_info;
+    BYTE buf[GAME_DRAWING_AREA_MEM_SIZE];
+} Game_Bitmap;
+
+typedef struct Pixel32 {
+    BYTE blue;
+    BYTE green;
+    BYTE red;
+    BYTE alpha;
+} Pixel32;
 
 BOOL game_running = FALSE;
+Game_Bitmap back_buffer;
 
 int WinMain(HINSTANCE inst, HINSTANCE prev_inst, PSTR cmd_line, INT cmd_show) {
     if (instance_already_running()) { 
@@ -16,27 +36,40 @@ int WinMain(HINSTANCE inst, HINSTANCE prev_inst, PSTR cmd_line, INT cmd_show) {
     }
 
     HWND window;
-    DWORD error_code = create_main_game_window(&window, inst);
-    if (error_code != ERROR_SUCCESS) {
+    if (create_main_game_window(&window, inst) != ERROR_SUCCESS) {
         MessageBoxA(NULL, "Window registration failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
+
+    int monitor_width, monitor_height;
+    if (get_monitor_resolution(&monitor_width, &monitor_height, window)) {
+        MessageBoxA(NULL, "Could not figure out monitor resolution!", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    back_buffer.bitmap_info.bmiHeader = (BITMAPINFOHEADER) {
+        .biSize = sizeof(back_buffer.bitmap_info.bmiHeader),
+        .biWidth = GAME_RES_WIDTH,
+        .biHeight = GAME_RES_HEIGHT,
+        .biBitCount = GAME_BPP,
+        .biCompression = BI_RGB,
+        .biPlanes = 1,
+    };
+    memset(back_buffer.buf, 255, sizeof(back_buffer.buf));
     
     game_running = TRUE;
     MSG msg;
     while (game_running) {
         while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) DispatchMessage(&msg);
         ProcessPlayerInput(window);
-        //RenderFrame();
+        RenderFrame(window);
         Sleep(1);
     }
-
 
     return 0;
 }
 
 LRESULT CALLBACK main_window_proc(HWND window, UINT msg, WPARAM w_param, LPARAM l_param) { 
-
     switch (msg) { 
     case WM_CLOSE:
         game_running = FALSE;
@@ -57,14 +90,15 @@ DWORD create_main_game_window(_Out_ HWND *window, _In_ HINSTANCE inst) {
         .hIcon = LoadIconA(NULL, IDI_APPLICATION),
         .hIconSm = LoadIconA(NULL, IDI_APPLICATION),
         .hCursor = LoadCursorA(NULL, IDC_ARROW),
-        .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+        .hbrBackground = CreateSolidBrush(RGB(255, 0, 255)),
         .lpszClassName = GAME_NAME "_WINDOWCLASS",
     };
     if (!RegisterClassExA(&window_class)) return GetLastError();
 
     *window = CreateWindowExA(WS_EX_CLIENTEDGE, window_class.lpszClassName, GAME_NAME, (WS_OVERLAPPEDWINDOW | WS_VISIBLE), 
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, inst, NULL);
+        CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, NULL, NULL, inst, NULL);
     if (!*window) return GetLastError();
+
     
     return ERROR_SUCCESS;
 }
@@ -79,4 +113,22 @@ void ProcessPlayerInput(HWND window) {
     if (GetAsyncKeyState(VK_ESCAPE)) {
         SendMessageA(window, WM_CLOSE, 0, 0);
     }
+}
+
+void RenderFrame(HWND window) {
+    HDC device_context = GetDC(window);
+
+    StretchDIBits(device_context, 0, 0, 100, 100, 0, 0, 100, 100, back_buffer.buf, &back_buffer.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+
+    ReleaseDC(window, device_context);
+}
+
+DWORD get_monitor_resolution(int *monitor_width, int *monitor_height, HWND window) {
+    MONITORINFO monitor_info = {sizeof(MONITORINFO)};
+    if (!GetMonitorInfoA(MonitorFromWindow(window, MONITOR_DEFAULTTOPRIMARY), &monitor_info)) return ERROR_MONITOR_NO_DESCRIPTOR;
+
+    *monitor_width = monitor_info.rcMonitor.right - monitor_info.rcMonitor.left;
+    *monitor_height = monitor_info.rcMonitor.bottom - monitor_info.rcMonitor.top;
+
+    return ERROR_SUCCESS;
 }
