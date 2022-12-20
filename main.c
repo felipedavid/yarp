6181c8a99158b2f0d7ee934232dc05a294c920bf
Felipe David <felipedavid.huh@gmail.com>
2022-12-20 02:39:03 -0300

    Actually finishing the process when the window is closed and making sure only one instace of the program is running

--- main.c
+++ main.c
@@ -1,31 +1,21 @@
 #include <windows.h>
 
-LRESULT CALLBACK main_window_proc(HWND window, UINT msg, WPARAM w_param, LPARAM l_param);
+#define GAME_NAME "YARP"
+
+LRESULT CALLBACK main_window_proc(_In_ HWND window, _In_ UINT msg, _In_ WPARAM w_param, _In_ LPARAM l_param);
+DWORD create_main_game_window(HINSTANCE instr);
+BOOL game_running(void);
 
 int WinMain(HINSTANCE inst, HINSTANCE prev_inst, PSTR cmd_line, INT cmd_show) {
-    WNDCLASSEXA window_class = {
-        .cbSize = sizeof(WNDCLASSEXA),
-        .style = 0,
-        .lpfnWndProc = main_window_proc,
-        .hInstance = inst,
-        .hIcon = LoadIconA(NULL, IDI_APPLICATION),
-        .hCursor = LoadCursorA(NULL, IDC_ARROW),
-        .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
-        .lpszClassName = "YARP",
-        .hIconSm = LoadIconA(NULL, IDI_APPLICATION),
-    };
-    if (!RegisterClassExA(&window_class)) {
-        MessageBoxA(NULL, "Window registration failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+    if (game_running()) { 
+        MessageBoxA(NULL, "Another instance of this program is already running!", "Error", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
-    HWND window = CreateWindowExA(WS_EX_CLIENTEDGE, window_class.lpszClassName, "YARP", WS_OVERLAPPEDWINDOW, 
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, inst, NULL);
-    if (!window) {
-        MessageBoxA(NULL, "Window creation failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+    if (create_main_game_window(inst) != ERROR_SUCCESS) {
+        MessageBoxA(NULL, "Window registration failed!", "Error", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
-    ShowWindow(window, TRUE);
 
     MSG msg;
     while (GetMessageA(&msg, NULL, 0, 0) > 0) {
@@ -37,21 +27,39 @@ int WinMain(HINSTANCE inst, HINSTANCE prev_inst, PSTR cmd_line, INT cmd_show) {
 }
 
 LRESULT CALLBACK main_window_proc(HWND window, UINT msg, WPARAM w_param, LPARAM l_param) { 
+
     switch (msg) { 
-        case WM_CREATE: 
-            return 0; 
- 
-        case WM_PAINT: 
-            return 0; 
- 
-        case WM_SIZE: 
-            return 0; 
- 
-        case WM_DESTROY: 
-            return 0; 
- 
-        default: 
-            return DefWindowProcA(window, msg, w_param, l_param); 
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        break;
+    default: 
+        return DefWindowProcA(window, msg, w_param, l_param); 
     } 
     return 0; 
 } 
+
+DWORD create_main_game_window(_In_ HINSTANCE inst) {
+    WNDCLASSEXA window_class = {
+        .cbSize = sizeof(WNDCLASSEXA),
+        .style = 0,
+        .lpfnWndProc = main_window_proc,
+        .hInstance = inst,
+        .hIcon = LoadIconA(NULL, IDI_APPLICATION),
+        .hIconSm = LoadIconA(NULL, IDI_APPLICATION),
+        .hCursor = LoadCursorA(NULL, IDC_ARROW),
+        .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+        .lpszClassName = GAME_NAME "_WINDOWCLASS",
+    };
+    if (!RegisterClassExA(&window_class)) return GetLastError();
+
+    HWND window = CreateWindowExA(WS_EX_CLIENTEDGE, window_class.lpszClassName, GAME_NAME, (WS_OVERLAPPEDWINDOW | WS_VISIBLE), 
+        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, inst, NULL);
+    if (!window) return GetLastError();
+    
+    return ERROR_SUCCESS;
+}
+
+BOOL game_running(void) {
+    CreateMutexA(NULL, FALSE, GAME_NAME "_MUTEX");
+    return GetLastError() == ERROR_ALREADY_EXISTS;
+}
